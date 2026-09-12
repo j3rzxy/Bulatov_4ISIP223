392067e1b5ddc19bf59c1f36afd144a058d1fb9f
234998 <234998@KB37-1-303-C04>
2026-09-12 11:03:41 +0300

    Проведен рефакторинг кода библиотеки и реализован главный блок программы.

--- PR1/BuildAnalyzer.App.cs
+++ PR1/BuildAnalyzer.App.cs
@@ -7,7 +7,28 @@ namespace PR1
     {
         static void Main(string[] args)
         {
+            Core player = new Core();
 
+            Console.WriteLine("Введите имя персонажа:");
+            player.CharName = Console.ReadLine();
+            Console.WriteLine("Введите базовую атаку:");
+            player.BaseAttack = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите атаку оружия:");
+            player.WeaponAttack = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите шанс критического удара:");
+            player.CritChance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите критический урон в %:");
+            player.CritDamageProc = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Имя вашего персонажа: {player.CharName}\n");
+            double ra = player.ResultAttckC(); 
+            Console.WriteLine($"Итоговая атака: {ra}");
+            double cd = player.CritDamageC();
+            Console.WriteLine($"Итоговая атака: {cd}");
+            double avg = player.AvgDmgC();
+            Console.WriteLine($"Итоговая атака: {avg}");
+            string rate = player.Rating();
+            Console.WriteLine($"Рейтинг сборки: {rate}");
         }
     }
 }
--- PR1_Library/BuildAnalyzer.Core.cs
+++ PR1_Library/BuildAnalyzer.Core.cs
@@ -2,42 +2,33 @@
 {
     public class Core
     {
-        public string CharName { get; set; }
+        public string CharName { get; set; } = string.Empty;
         public double BaseAttack { get; set; }
         public double WeaponAttack { get; set; }
         public double CritChance { get; set; }
         public double CritDamageProc { get; set; }
-        private double ResultDMG;
-        private double ResultCritDMG;
-        private double AvgDmg;
         public Core(string charName, double baseAttack, double weaponAttack, double critChance, double critDamageProc)
         {
             CharName = charName;
             BaseAttack = baseAttack;
             WeaponAttack = weaponAttack;
             CritChance = critChance;
-            CritDamageProc = critDamageProc;
-            ResultDMG = BaseAttack + WeaponAttack;
-            ResultCritDMG = ResultDMG * (1 + (CritDamageProc / 100));
-            AvgDmg = ResultDMG * (1 + (CritChance / 100) * (ResultCritDMG / 100));
+            CritDamageProc = critDamageProc;            
         }
 
         public Core()
         {
         }
+        public double ResultDMG => BaseAttack + WeaponAttack;
 
-        public double ResultAttckC()
-        {
-            return ResultDMG;
-        }
-        public double CritDamageC()
-        {
-            return ResultCritDMG;
-        }
-        public double AvgDmgC()
-        {
-            return AvgDmg;
-        }
+        public double ResultCritDMG => ResultDMG * (1 + (CritDamageProc / 100));
+
+        public double AvgDmg => ResultDMG * (1 - (CritChance / 100)) + (ResultCritDMG * (CritChance / 100));
+
+        public double ResultAttckC() => ResultDMG;
+        public double CritDamageC() => ResultCritDMG;
+        public double AvgDmgC() => AvgDmg;
+
         public string Rating()
         {
             if (AvgDmgC() < 1500)
