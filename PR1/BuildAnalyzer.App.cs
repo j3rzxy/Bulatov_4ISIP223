764c58e7aec12d5958594a1fe43efd1290c6ddef
234998 <234998@KB37-1-303-C04>
2026-09-12 11:07:22 +0300

    Исправлен вывод программы.

--- PR1/BuildAnalyzer.App.cs
+++ PR1/BuildAnalyzer.App.cs
@@ -19,14 +19,15 @@ namespace PR1
             player.CritChance = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Введите критический урон в %:");
             player.CritDamageProc = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(" ");
 
             Console.WriteLine($"Имя вашего персонажа: {player.CharName}\n");
             double ra = player.ResultAttckC(); 
             Console.WriteLine($"Итоговая атака: {ra}");
             double cd = player.CritDamageC();
-            Console.WriteLine($"Итоговая атака: {cd}");
+            Console.WriteLine($"Итоговый критический урон: {cd}");
             double avg = player.AvgDmgC();
-            Console.WriteLine($"Итоговая атака: {avg}");
+            Console.WriteLine($"Средний урон: {avg}");
             string rate = player.Rating();
             Console.WriteLine($"Рейтинг сборки: {rate}");
         }
