2c0e9200398fe7c7f6d021ac1e1cc22a4a75224b
234998 <234998@KB37-1-303-C04>
2026-09-12 10:50:25 +0300

    Создана библиотека с основными методами программы.

--- PR1/BuildAnalyzer.App.cs
+++ PR1/BuildAnalyzer.App.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BuildAnalyzer.Core;
 
 namespace PR1
 {
@@ -10,6 +7,7 @@ namespace PR1
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
---
+++ PR1_Library/BuildAnalyzer.Core.cs
@@ -0,0 +1,61 @@
+﻿namespace BuildAnalyzer.Core
+{
+    public class Core
+    {
+        public string CharName { get; set; }
+        public double BaseAttack { get; set; }
+        public double WeaponAttack { get; set; }
+        public double CritChance { get; set; }
+        public double CritDamageProc { get; set; }
+        private double ResultDMG;
+        private double ResultCritDMG;
+        private double AvgDmg;
+        public Core(string charName, double baseAttack, double weaponAttack, double critChance, double critDamageProc)
+        {
+            CharName = charName;
+            BaseAttack = baseAttack;
+            WeaponAttack = weaponAttack;
+            CritChance = critChance;
+            CritDamageProc = critDamageProc;
+            ResultDMG = BaseAttack + WeaponAttack;
+            ResultCritDMG = ResultDMG * (1 + (CritDamageProc / 100));
+            AvgDmg = ResultDMG * (1 + (CritChance / 100) * (ResultCritDMG / 100));
+        }
+
+        public Core()
+        {
+        }
+
+        public double ResultAttckC()
+        {
+            return ResultDMG;
+        }
+        public double CritDamageC()
+        {
+            return ResultCritDMG;
+        }
+        public double AvgDmgC()
+        {
+            return AvgDmg;
+        }
+        public string Rating()
+        {
+            if (AvgDmgC() < 1500)
+            {
+                return "Weak";
+            }
+            else if (AvgDmgC() >= 1500 && AvgDmgC() < 2499)
+            {
+                return "Normal";
+            }
+            else if (AvgDmgC() >= 2500 && AvgDmgC() < 3499)
+            {
+                return "Good";
+            }
+            else
+            {
+                return "Great";
+            }
+        } 
+    }
+}
